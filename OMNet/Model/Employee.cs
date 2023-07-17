afb1de42bb30243b68c1498bea6596ae03855c01
srkans <serkansacma@hotmail.com>
2023-07-17 11:04:14 +0300

    update and delete

--- OMNet/Model/Employee.cs
+++ OMNet/Model/Employee.cs
@@ -9,12 +9,18 @@ namespace Model
         [RedisIdField]
         [Indexed]
         public string? Id { get; set; }
+
         [Indexed]
         public List<string>? Sales { get; set; }
+
         [Indexed(JsonPath = "$.Location")]
         [Indexed(JsonPath = "$.PostalCode")]
         public Address? Address { get; set; }
-        [Indexed]
+
+        [Indexed(Sortable = true)]
         public string? Name { get; set; }
+
+        [Indexed(Sortable = true)]
+        public int Age { get; set; }
     }
 }
